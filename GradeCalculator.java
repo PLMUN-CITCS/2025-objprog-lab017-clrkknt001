3ff0ad9aaef5cc572530c97249cc71a79920ee11
Clark <magaclarkkent_bscs@plmun.edu.com>
2025-03-02 17:57:53 +0800

    Submitting OBJPROG Week 05 - Laboratory # 17

---
+++ GradeCalculator.java
@@ -0,0 +1,38 @@
+package OOB;
+import java.util.Scanner;
+
+public class GradeCalculator {
+
+	public static void main(String[] args) {
+		int score = getStudentScore();
+        String grade = calculateGrade(score);
+        System.out.println("Your grade is: " + grade);
+
+	}
+	
+	public static int getStudentScore() {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter your score: ");
+    	int score = s.nextInt();
+    	s.close();
+    	return score;
+		
+	}
+	
+	public static String calculateGrade(int score) {
+        if (score >= 90) {
+            return "A";
+        } else if (score >= 80) {
+            return "B";
+        } else if (score >= 70) {
+            return "C";
+        } else if (score >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+	}
+	
+	}
+		
+
